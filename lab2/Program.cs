7aa3e1f973a91c10a34557650628f006fe0d3a3d
bolshakovartem <89931102+bolshakovartem@users.noreply.github.com>
2021-10-19 13:12:39 +0300

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] decData = File.ReadAllBytes("data.txt").ToArray();
+            foreach(byte i in decData)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var genPassword = new RNGCryptoServiceProvider();
+            byte[] password = new byte[decData.Length];
+            genPassword.GetBytes(password);
+            foreach (byte i in password)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            byte[] encData = new byte[decData.Length];
+            for(int i=0; i<decData.Length; i++)
+            {
+                encData[i] = (byte)(decData[i] ^ password[i]);
+            }
+
+            File.WriteAllBytes("encdata.dat", encData);
+
+            byte[] Text = File.ReadAllBytes("encdata.dat").ToArray();
+            byte[] decText = new byte[Text.Length];
+            for(int i=0; i<Text.Length; i++)
+            {
+                decText[i] = (byte)(Text[i] ^ password[i]);
+            }
+
+            Console.Write(Encoding.UTF8.GetString(decText));
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
