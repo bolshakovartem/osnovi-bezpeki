bd8754779ec2175a3e725eb6578457bf001c9170
bolshakovartem <89931102+bolshakovartem@users.noreply.github.com>
2021-10-24 16:35:24 +0300

    Add files via upload

---
+++ lab3/lab3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace lab3._2
+{
+    class Program
+    {
+        static byte[] ComputeHashMd5(byte[] dataForHash)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(dataForHash);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Guid guid1 = new Guid("564c8da6-0440-88ec-d453-0bbad57c6036");
+            string a = 12345678.ToString();
+            for(int i=100000000;i<200000000;i++)
+            {
+
+            }
+        }
+    }
+}
