4cd3df71c419d4e59c8062fb9eeee7f7716709b5
bolshakovartem <89931102+bolshakovartem@users.noreply.github.com>
2021-11-02 09:57:50 +0200

    Add files via upload

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5
+{
+    public class Hash
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+
+        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Combine(toBeHashed, salt));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "it-is-raining";
+            byte[] salt = Hash.GenerateSalt();
+            Console.WriteLine("Password : " + password);
+            Console.WriteLine("Salt = " + Convert.ToBase64String(salt));
+            Console.WriteLine();
+            var hashedPassword1 = Hash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt);
+            Console.WriteLine("Hashed Password = " + Convert.ToBase64String(hashedPassword1));
+            Console.ReadLine();
+        }
+    }
+}
