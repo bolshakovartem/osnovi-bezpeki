a7171b9575f33cd43e6f4ddfdbe46ac34ed1804c
bolshakovartem <89931102+bolshakovartem@users.noreply.github.com>
2021-11-19 10:02:43 +0200

    Add files via upload

---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab8
+{
+    class Program
+    {
+        private readonly static string CspContainerName = "RSAKeyContainer";
+        public static void AssignNewKey(string publicKeyPath)
+        {
+            CspParameters cspParameters = new CspParameters(1)
+            {
+                KeyContainerName = CspContainerName,
+
+                ProviderName = "Microsoft Strong Cryptographic Provider"
+            };
+            var rsa = new RSACryptoServiceProvider(cspParameters)
+            {
+                PersistKeyInCsp = true
+            };
+            File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+        }
+        public static byte[] EncryptData(string publicKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+            return cipherbytes;
+        }
+        public static byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plainBytes;
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = CspContainerName,
+            };
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.PersistKeyInCsp = true;
+                plainBytes = rsa.Decrypt(dataToDecrypt, false);
+            }
+            return plainBytes;
+        }
+        
+        static void Main(string[] args)
+        {
+            //byte[] data = File.ReadAllBytes("secretMessage.dat").ToArray();
+            //string data1 = BitConverter.ToString(data);
+            string data = "Hi! My name is Artem. This is my message";
+            AssignNewKey("BolshakovArtem.xml");
+            var encrypted = EncryptData("BolshakovDenis.xml", Encoding.Unicode.GetBytes(data));
+            //var decrypted = DecryptData(encrypted);
+            File.WriteAllBytes("encryptMessage.dat", encrypted);
+            Console.WriteLine(" Original Text = " + data);
+            Console.WriteLine();
+            Console.WriteLine(" Encrypted Text = " + Convert.ToBase64String(encrypted));
+            //Console.WriteLine(" Decrypted Text = " + Encoding.Default.GetString(decrypted));
+        }
+    }
+}
